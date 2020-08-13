186cfc1a6b65d6c34d06eba7ee5ced8e032b490f
Neeharikaa <50858726+Neeharikaa@users.noreply.github.com>
2020-08-13 12:44:57 +0530

    Update point.c
    hi added
    Thank you added

--- point.c
+++ point.c
@@ -4,12 +4,13 @@ void main(){
 int a[]={12,23,23,45,34};
 int *p,sum=0;
 int a=10l;
-printf("hello");
+printf("hello hi");
  
 for(p=a+4;p>=a;p--){
         printf("%d \n",*p);
 
 }
+ printf("thankyou");
 
 
 }
